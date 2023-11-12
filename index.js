a3c9c7adcd3b91eb0a20792f09ef50c73c6b3cc9
Aug Lee <augie@AugMacPro.local>
2023-11-13 03:24:35 +0800

    Initial Commit

--- index.js
+++ index.js
@@ -1,41 +1,64 @@
-// Define variables for DOM elements
-const imageUpload = document.getElementById("image-upload");
-const memeImage = document.getElementById("meme-image");
-const topText = document.getElementById("top-text");
-const bottomText = document.getElementById("bottom-text");
-const generateMemeButton = document.getElementById("generate-meme");
+import express from 'express';
+import multer from 'multer'; // For handling file uploads
+// import { fileURLToPath } from 'url';
+// import { dirname } from 'path';
+// import path from 'path';
+// import bodyParser from "body-parser";
 
-// Event listener for image upload
-imageUpload.addEventListener("change", () => {
-    const file = imageUpload.files[0];
-    if (file) {
-        const imageURL = URL.createObjectURL(file);
-        memeImage.src = imageURL;
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+// app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.set('view engine', 'ejs');
+app.use(express.json());
+
+// Configure multer to handle file uploads
+const storage = multer.diskStorage({
+    destination: __dirname + "/public/images",
+    filename: function (req, file, cb) {
+        cb(null, file.originalname); // Use the original file name
     }
 });
 
-// Event listener for generating the meme
-generateMemeButton.addEventListener("click", () => {
-    // Remove existing text elements
-    document.querySelectorAll(".meme-text").forEach((element) => {
-        element.remove();
-    });
+const upload = multer({ storage: storage });
 
-    // Create top text element
-    const topTextValue = topText.value;
-    const topTextDiv = document.createElement("div");
-    topTextDiv.className = "meme-text top";
-    topTextDiv.innerText = topTextValue;
-    memeImage.insertAdjacentElement("beforebegin", topTextDiv);
+// Routes
+app.get('/', (req, res) => {
+    console.log(req.file);
+    res.render('index.ejs', { imageURL: "", topTextValue: "", bottomTextValue: "" });
+});
 
-    // Create bottom text element
-    const bottomTextValue = bottomText.value;
-    const bottomTextDiv = document.createElement("div");
-    bottomTextDiv.className = "meme-text bottom";
-    bottomTextDiv.innerText = bottomTextValue;
-    memeImage.insertAdjacentElement("afterend", bottomTextDiv);
+app.get('/about', (req, res) => {
+    res.render('about.ejs');
+});
 
-    // Clear textareas
-    topText.value = "";
-    bottomText.value = "";
+app.get('/contact', (req, res) => {
+    res.render('contact.ejs');
+});
+
+// Handle form submission
+app.post('/generate', upload.single('image'), (req, res) => {
+    const topText = req.body.topText;
+    const bottomText = req.body.bottomText;
+
+    // Check if an image was uploaded
+    if (req.file) {
+        res.render('index.ejs', { imageURL: "images/" + req.file.filename, topTextValue: topText, bottomTextValue: bottomText });
+    } else {
+        // Handle the case where no image was uploaded
+        res.render('index.ejs', { topTextValue: topText, bottomTextValue: bottomText });
+    }
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
